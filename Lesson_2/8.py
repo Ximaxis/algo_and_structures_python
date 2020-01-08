52dfeac9c6f0be8c8cc6f8a7b59c1b9aae2433e0
Ximaxis <57436547+Ximaxis@users.noreply.github.com>
2020-01-08 16:10:05 +0400

    Update 8

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -4,32 +4,41 @@
  которую необходимо посчитать, задаются вводом с клавиатуры.
 """
 
-
-NUMBER = int(input('Введите число: '))
+COUNT_NUMBER = int(input('Введите количество чисел которые будем проверять: '))
 FIND = int(input('Введите цифру которую ищем: '))
 
-def search(N, F, I = 0):
-    if N < 1:
-        return I
+def search(n, f, i = 0):
+    if n < 1:
+        return i
     else:
-        if N % 10 == F:
-            I += 1
+        if n % 10 == f:
+            i += 1
         else:
             pass
-    return search((N // 10), F, I)
+    return search((n // 10), f, i)
 
-print(f"В числе {NUMBER} цифра {FIND} встраечается {search(NUMBER,FIND)} раз")
+def count_number(c, f, i = 0):
+    if c == 0:
+        return i
+    else:
+        number = int(input(f'Введите {c} число которые будем проверять: '))
+        i += search(number, f)
+        return count_number(c - 1, f, i)
+
+print(f"В заданой последоватеельности цифра {FIND} встраечается {count_number(COUNT_NUMBER, FIND)} раз")
 
 """ 
 Решение циклом
 
-number = input('Введите число: ')
+count_number = int(input('Введите количество искомых чисел: '))
 find = int(input('Введите число которое ищем: '))
 count = 0
-for i in number:
-    if int(i) == find:
-        count += 1
+while count_number != 0:
+    number = input('Введите число: ')
+    count_number -= 1
+    for i in number:
+        if int(i) == find:
+            count += 1
 
 print("Число %d нашлось %d раз(а)" % (find, count))
-
 """
