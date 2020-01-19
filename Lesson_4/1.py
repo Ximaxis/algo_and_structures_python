0deed8405a9b1f95c26aa484230b75293814bcd4
Ximaxis <57436547+Ximaxis@users.noreply.github.com>
2020-01-19 23:49:59 +0400

    HW4

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,71 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+import cProfile
+import timeit
+import random
+
+# Первый алгоритм
+
+def count_number(quantity, array):
+    """Функция перебирает все элементы массива и выводит тот что встречается чаще"""
+    number = array[0]
+    max_count = 1
+    for idx in range(quantity - 1):
+        cur_count = 1
+        for cnt in range(idx + 1, quantity):
+            if array[idx] == array[cnt]:
+                cur_count += 1
+        if cur_count > max_count:
+            max_count = cur_count
+            number = array[idx]
+    return max_count, number
+
+
+def start1():
+    """функция создает массив, и выводит реультат проверки"""
+    quantity = 100
+    array = [0] * quantity
+    for i in range(quantity):
+        array[i] = int(random.random() * 100)
+
+    max_count, number = count_number(quantity, array)
+    if max_count > 1:
+        pass
+    else:
+        pass
+
+# Второй алгоритм
+
+def start2():
+    """функция перебирает все элементы массива
+    и записывает число с максимальным количеством повторений"""
+    list = [random.randint(-100, 100) for i in range(100)]
+    num = max(list, key=list.count)
+
+
+# print(timeit.timeit(start1))
+# print(timeit.timeit(start2))
+
+print(timeit.timeit('start1()',
+                    setup='from __main__ import start1',
+                    number=10000))
+
+print(timeit.timeit('start2()',
+                    setup='from __main__ import start2',
+                    number=10000))
+"""
+Взял алгоритм с 3 урока, считающий максимальное количество появление числа
+Алгоритм start1 имеет сложность квадратичную O(n)^2 а start2 имеет линейную сложность О(n)
+если их сравнить по получается следующая закономерность:
+при числе элементов 5 start1 = 8.96 сек start2 = 16.5 сек
+без ограничения запусков при большем числе элементов выходит очень долго,
+потому введу ограничение на 10000 запусков
+при числе элементов 5 start1 = 0.075 сек start2 = 0.139 сек
+при числе элементов 20 start1 = 0.56 сек start2 = 0.73 сек
+при числе элементов 50 start1 = 2.59 сек start2 = 1.92 сек
+при числе элементов 100 start1 = 8.64 сек start2 = 4.61 сек
+и для завершения
+при числе элементов 1000 start1 = 768.47 сек start2 = 266.61 сек
+"""
+cProfile.run(f"start2()")
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,63 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+import timeit
+
+
+def simple_number_list(desired):
+    n = desired * 8
+    lst = [2]
+    for i in range(3, n + 1, 2):
+        if (i > 10) and (i % 10 == 5):
+            continue
+        for j in lst:
+            if j * j - 1 > i:
+                if len(lst) == desired:
+                    return lst[desired - 1]
+                else:
+                    lst.append(i)
+                break
+            if i % j == 0:
+                break
+        else:
+            lst.append(i)
+
+
+def sieve_eratosfen(desired):
+    max_num = desired * 8
+    n = 2
+    lst = list(range(max_num + 1))
+    lst[1] = 0
+    while n < max_num:
+        if lst[n] != 0:
+            m = n * 2
+            while m < max_num:
+                lst[m] = 0
+                m += n
+        n += 1
+    lst2 = []
+    for i in lst:
+        if lst[i] != 0:
+            lst2.append(lst[i])
+    del lst
+
+    return lst2[desired - 1]
+
+
+DESIRED = int(input('Ведите какое по счету простое число требуется найти '))
+if DESIRED <= 1000:
+    print(f'Простое число под номером {DESIRED} это: {simple_number_list(DESIRED)}')
+    print(f'Простое число под номером  {DESIRED} это: {sieve_eratosfen(DESIRED)}')
+    print(timeit.timeit('simple_number_list(DESIRED)',
+                    setup='from __main__ import simple_number_list, DESIRED', number=1000))
+    print(timeit.timeit('sieve_eratosfen(DESIRED)',
+                    setup='from __main__ import sieve_eratosfen, DESIRED', number=1000))
+else:
+    print(f'Число {DESIRED} слишком велико')
+
+"""Ввел намереное ограничение лимита вводимого числа,
+чтобы оно не превышало возможное максимальное число в ряду чисел
+в ином случае в функции с решетом создавался огромный список лишних чисел,
+что тормозило расчеты в разы на функции с ним,
+теперь же решето уже после 400 значения начинает работать быстрее алгоритма без решета
+"""
