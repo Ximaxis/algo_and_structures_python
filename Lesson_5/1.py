25a8c913386ae4dfadb51bd1710565f466e3988a
Ximaxis <57436547+Ximaxis@users.noreply.github.com>
2020-01-22 21:28:24 +0400

    HW5

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,51 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+from collections import namedtuple
+CI = namedtuple('Company', 'name First Second Third Fourth')
+
+
+def input_namedtuple():
+    """Функция запрашивает данные для заполнения именного кортежа"""
+    company_info = CI(name=input("Введите название компании: "),
+                      First=int(input("Введите прибыль за первый квартал: ")),
+                      Second=int(input("Введите прибыль за второй квартал: ")),
+                      Third=int(input("Введите прибыль за третий квартал: ")),
+                      Fourth=int(input("Введите прибыль за четвертый квартал: ")))
+    return company_info
+
+
+def average_income(info, count):
+    """Функция считае среднюю прибыль по всем компаниям"""
+    total_income = 0
+    for company in range(count):
+        total_income += (info[company].First + info[company].Second + info[company].Third +
+                         info[company].Fourth)
+    return total_income / count
+
+
+def company_income(company):
+    """Общая прибыль у отдеьно взятой фирмы"""
+    return company.First + company.Second + company.Third + company.Fourth
+
+
+def above_average_company(info, avg_res, count):
+    """Отсортировка по двум спискам, компании с доходом выше среднего и оставшиеся(ниже среднего)"""
+    best = []
+    residue = []
+    for i in range(count):
+        if company_income(info[i]) > avg_res:
+            best.append(info[i].name)
+        else:
+            residue.append(info[i].name)
+    return best, residue
+
+
+COUNT = int(input('Введите количество компаний для расчетов: '))
+INFO = [(input_namedtuple()) for i in range(COUNT)]
+# print(INFO)
+AVG_RES = average_income(INFO, COUNT)
+print(f"Средняя прибыль {AVG_RES}/год")
+BESTS, REST = above_average_company(INFO, AVG_RES, COUNT)
+print(f"Прибыль выше среднего у компании(й) {BESTS}"
+      f"\n Прибыль ниже среднего у компании(й) {REST}")
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,28 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+import collections
+from functools import reduce
+
+
+def summ(num_list):
+    """Функция, переводит числа в десятичные,
+    суммирует их и возвращает в виде шестнадцатиричного результата"""
+    return hex(sum([int(''.join(i), 16) for i in num_list.values()]))[2:]
+
+
+def multiply(num_list):
+    """Функция, переводит числа в десятичные,
+    перемножает их и возвращает в виде шестнадцатиричного результата"""
+    return hex(reduce(lambda a, b: a * b, [int(''.join(i), 16) for i in num_list.values()]))[2:]
+
+
+if __name__ == '__main__':
+    NUM_LIST = collections.defaultdict(list)
+
+    for n in range(1, 3):
+        d = input(f'Введите {n}-е шестнадцатеричное число: ')
+        NUM_LIST[n] = list(d)
+    # print(NUM_LIST)
+
+    print(f'Сумма чисел = {summ(NUM_LIST)}\nПроизведение чисел = {multiply(NUM_LIST)}')
